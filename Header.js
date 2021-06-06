4aa985e5525badb4a92b2acb0507379cbaee0f5f
Walter Epp <woltz89@gmail.com>
2021-06-06 18:11:14 +0200

    new file

---
+++ Header.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./Header.css";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const [inputSearch, setInputSearch] = useState("");
+
+  return (
+    <div className="header">
+      <div className="header__left">
+        <MenuIcon />
+        <Link to="/">
+          <img
+            className="header__logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1280px-YouTube_Logo_2017.svg.png"
+            alt=""
+          />
+        </Link>
+      </div>
+
+      <div className="header__input">
+        <input
+          onChange={(e) => setInputSearch(e.target.value)}
+          value={inputSearch}
+          placeholder="Suche"
+          type="text"
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header__inputButton" />
+        </Link>
+      </div>
+
+      <div className="header__icons">
+        <VideoCallIcon className="header__Icon" />
+        <AppsIcon />
+        <NotificationsIcon />
+        <Avatar
+          alt="Walter Epp"
+          src="https://profile-images.xing.com/images/15fa853e455156400f8cd333bef0b4c2-1/walter-epp.1024x1024.jpg"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
